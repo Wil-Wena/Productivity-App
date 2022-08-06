59d758c8e77483c1342f10d7f8c5d908d010dc1d
Wilson Aballey <willywallet001@gmail.com>
2022-08-06 16:15:12 +0000

    Buttons for My Controller App

---
+++ lib/widgets/ProductivityButton/ProductivityButton.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class ProductivityButton extends StatelessWidget {
+  final Color color;
+  final String text;
+  final double size;
+  final VoidCallback onPressed;
+  const ProductivityButton(
+      {Key? key,
+      required this.color,
+      required this.text,
+      required this.size,
+      required this.onPressed})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FlatButton(
+      onPressed: this.onPressed,
+      child: Text(
+        this.text,
+        style: TextStyle(color: Colors.white),
+      ),
+      color: this.color,
+      minWidth: this.size,
+    );
+  }
+}
