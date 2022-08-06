2684ad16a537d2ddbbb1382d83e604cfd526f51e
Wilson Aballey <willywallet001@gmail.com>
2022-08-06 19:03:32 +0000

    Percent Indicator Feature

--- lib/Pages/TimeHomePage.dart
+++ lib/Pages/TimeHomePage.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:percent_indicator/circular_percent_indicator.dart';
 import 'package:productivity_timer/main.dart';
 import 'package:productivity_timer/widgets/ProductivityButton.dart';
 
@@ -13,65 +13,85 @@ class TimerHomePage extends StatelessWidget {
     return SafeArea(
       child: Scaffold(
           appBar: AppBar(
-            title: const Text("My time Controller"),
+            title: const Text("Task Scheduler"),
           ),
-          body: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(5.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    defaultPadding,
-                    const Expanded(
-                        child: ProductivityButton(
-                            color: Colors.blueAccent,
-                            text: "Work",
-                            size: 23.5,
-                            onPressed: emptyMethod)),
-                    defaultPadding,
-                    const Expanded(
-                        child: ProductivityButton(
-                            color: Colors.blueAccent,
-                            text: "Short Break",
-                            size: 23.5,
-                            onPressed: emptyMethod)),
-                    defaultPadding,
-                    const Expanded(
-                        child: ProductivityButton(
-                            color: Colors.blueAccent,
-                            text: "Long Break",
-                            size: 23.5,
-                            onPressed: emptyMethod)),
-                  ],
-                ),
-              ),
-              const Expanded(child: Text("Timer Under Construction")),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
-                  children: [
-                    defaultPadding,
-                    const Expanded(
-                      child: ProductivityButton(
-                          color: Colors.deepPurple,
-                          text: 'Stop',
-                          size: 40.0,
-                          onPressed: emptyMethod),
+          body: LayoutBuilder(
+            builder: (BuildContext context, BoxConstraints constraints) {
+              final double availableWidth = constraints.maxWidth;
+              return Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(5.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        defaultPadding,
+                        const Expanded(
+                            child: ProductivityButton(
+                                color: Colors.blueAccent,
+                                text: "Work",
+                                size: 23.5,
+                                onPressed: emptyMethod)),
+                        defaultPadding,
+                        const Expanded(
+                            child: ProductivityButton(
+                                color: Colors.deepPurple,
+                                text: "Short Break",
+                                size: 23.5,
+                                onPressed: emptyMethod)),
+                        defaultPadding,
+                        const Expanded(
+                            child: ProductivityButton(
+                                color: Colors.blueGrey,
+                                text: "Long Break",
+                                size: 23.5,
+                                onPressed: emptyMethod)),
+                      ],
                     ),
-                    defaultPadding,
-                    const Expanded(
-                      child: ProductivityButton(
-                          color: Colors.deepPurple,
-                          text: 'Restart',
-                          size: 40.0,
-                          onPressed: emptyMethod),
+                  ),
+                  const Spacer(),
+                  Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: Expanded(
+                        child: CircularPercentIndicator(
+                      radius: availableWidth / 3,
+                      lineWidth: 10.0,
+                      percent: 1,
+                      center: Text(
+                        "30.00",
+                        style: Theme.of(context).textTheme.headline4,
+                      ),
+                      progressColor: Colors.deepPurpleAccent,
+                    )),
+                  ),
+                  const Spacer(),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      children: [
+                        defaultPadding,
+                        const Expanded(
+                          child: ProductivityButton(
+                              color: Colors.deepPurple,
+                              text: 'Stop',
+                              size: 40.0,
+                              onPressed: emptyMethod),
+                        ),
+                        defaultPadding,
+                        const Expanded(
+                          child: ProductivityButton(
+                              color: Colors.blueAccent,
+                              text: 'Restart',
+                              size: 40.0,
+                              onPressed: emptyMethod),
+                        ),
+                      ],
                     ),
-                  ],
-                ),
-              )
-            ],
+                  )
+                ],
+              );
+            },
           )),
     );
   }
